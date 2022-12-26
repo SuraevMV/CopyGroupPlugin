e9f694934546c3e0bf9042af62b5a4df4714a9ea
SuraevMV <96659813+SuraevMV@users.noreply.github.com>
2022-12-27 01:06:52 +0300

    Добавьте файлы проекта.

---
+++ CopyGroup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+
+namespace CopyGroupPlugin
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class CopyGroup : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uIDoc = commandData.Application.ActiveUIDocument;
+            Document doc = uIDoc.Document;
+
+            Reference reference = uIDoc.Selection.PickObject(ObjectType.Element, "Выберите группу объектов");
+            Element element = doc.GetElement(reference);
+            Group group = element as Group;
+
+            XYZ point = uIDoc.Selection.PickPoint("Выберите точку");
+
+            Transaction transaction = new Transaction(doc);
+            transaction.Start("Копирование группы объектов");
+            doc.Create.PlaceGroup(point, group.GroupType);
+            transaction.Commit();
+
+            return Result.Succeeded;
+        }
+    }
+}
